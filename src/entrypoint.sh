900a755a9cdd8e2c3d7161d5bbeda7b8649f8173
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-02-02 19:06:16 +0000

    Template policies for Jenkins to Conjur

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 
+set -x
+set -e
+
 # execute the /entrypoint.sh script
 # this script is provided by the base image
 
 /entrypoint.sh
 
-conjur policy load -b root -f ./policy/root.yml
+conjur policy replace -b root -f ./policies/root/policy.yml
+conjur policy replace -b conjur -f ./policies/root/conjur/policy.yml
+conjur policy replace -b ci -f ./policies/root/ci/policy.yml
+conjur policy replace -b ci/jenkins -f ./policies/root/ci/jenkins/policy.yml
+conjur policy replace -b conjur/authn-jwt -f ./policies/root/conjur/authn-jwt/policy.yml
+conjur policy replace -b conjur/authn-jwt/jenkins -f ./policies/root/conjur/authn-jwt/jenkins/policy.yml
+# conjur policy replace -b ci/jenkins/projects -f ./policies/root/ci/jenkins/projects/policy.yml
 
 exec "$@"
