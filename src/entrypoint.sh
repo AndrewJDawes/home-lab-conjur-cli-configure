70f6861e9812e99b264a1380a9dfd56960276fcd
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-02-02 20:40:45 +0000

    Have it just base claim on whether it is FROM Jenkins and FOR cyberark-conjur

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -16,13 +16,12 @@ conjur policy replace -b conjur/authn-jwt -f ./policies/root/conjur/authn-jwt/po
 conjur policy replace -b conjur/authn-jwt/jenkins -f ./policies/root/conjur/authn-jwt/jenkins/policy.yml
 # conjur policy replace -b ci/jenkins/projects -f ./policies/root/ci/jenkins/projects/policy.yml
 
-conjur variable set -i conjur/authn-jwt/jenkins/token-app-property -v sub
+# conjur variable set -i conjur/authn-jwt/jenkins/token-app-property -v sub
+conjur variable set -i conjur/authn-jwt/jenkins/token-app-property -v aud
 # conjur variable set -i conjur/authn-jwt/jenkins/identity-path -v 'myspace/jwt-apps'
-conjur variable set -i conjur/authn-jwt/jenkins/identity-path -v 'ci/jenkins/controller'
+conjur variable set -i conjur/authn-jwt/jenkins/identity-path -v 'ci/jenkins'
 conjur variable set -i conjur/authn-jwt/jenkins/issuer -v 'http://localhost:8080'
 conjur variable set -i conjur/authn-jwt/jenkins/jwks-uri -v 'http://jenkins:8080/jwtauth/conjur-jwk-set'
 conjur variable set -i conjur/authn-jwt/jenkins/audience -v "cyberark-conjur"
-
-# 'host/ci/jenkins/controller/jobs-v2' not found
 
 exec "$@"
