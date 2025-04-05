79a09c6c3f05d98bdfcc09e3d1be9a0c5e479224
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-04-05 20:22:56 +0000

    Add umerx/ghcr

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -18,6 +18,8 @@ conjur policy replace -b codekaizen -f ./policies/root/codekaizen/policy.yml
 conjur policy replace -b codekaizen/website -f ./policies/root/codekaizen/website/policy.yml
 conjur policy replace -b umerx -f ./policies/root/umerx/policy.yml
 conjur policy replace -b umerx/npm -f ./policies/root/umerx/npm/policy.yml
+conjur policy replace -b umerx/ghcr -f ./policies/root/umerx/ghcr/policy.yml
+
 
 # The jwt's token-app-property (in this case, "aud") value will be appended to the identity-path value to form the "!host" that Conjur will authenticate the JWT token against.
 # So for example, if the jwt token has an "aud" value of "cyberark-conjur", the identity-path value is "ci/jenkins", then the Conjur host that the JWT token will be authenticated against is "ci/jenkins/cyberark-conjur".
